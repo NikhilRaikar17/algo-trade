3bcbf81bd27f39ff78d1877d9bf9ef79629ba743
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-10-26 17:52:02 +0100

    UPdated workflow

--- send_email.py
+++ send_email.py
@@ -5,10 +5,16 @@ from email.mime.base import MIMEBase
 from email import encoders
 import os
 from datetime import datetime
+from dotenv import load_dotenv
+
+ENV_FILE = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path=ENV_FILE, override=True)
 
 
 sender_email = os.getenv("GMAIL_USERNAME")
 receiver_email = os.getenv("RECEIVER_EMAIL")
+receiver_emails = receiver_email.split(",")  # split into list
+receiver_emails = [email.strip() for email in receiver_emails]  # clean spaces
 password = os.getenv("GMAIL_APP_PASSWORD")
 subject = "Algo paper trading results"
 body = "Hey Nikhil/Bharath, Please find attached the CSV file containing the latest paper trade results."
@@ -20,7 +26,7 @@ filename = f"AlgoTrade.xlsx"
 # Create a multipart message and set headers
 message = MIMEMultipart()
 message["From"] = sender_email
-message["To"] = receiver_email
+message["To"] = ", ".join(receiver_email)
 message["Subject"] = subject
 
 # Attach the body with the msg instance
@@ -45,8 +51,8 @@ part.add_header(
 message.attach(part)
 
 # Log in to the server and send the email
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login(sender_email, password)
-server.sendmail(sender_email, receiver_email, message.as_string())
-server.quit()
+with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    server.starttls()
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+print("Email sent successfully!")
